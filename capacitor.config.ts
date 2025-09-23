8992a9f7e322f62467efdf79992293ff79cfed66
PurpleCre <pgonese6@gmail.com>
2025-09-23 11:17:55 +0200

    capacitor intergration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pogosgpc.app',
+  appName: 'pogos loyalty',
+  webDir: 'dist'
+};
+
+export default config;
